eaae774d6e8b6e0352df819f12d146c784896a04
zidane <huuvi168@gmail.com>
2022-05-08 12:09:15 +0700

    add scss, add static file

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
-const express = require('express')
-const path = require('path')
-const morgan = require('morgan')
-const { engine }  = require('express-handlebars')
-const app = express()
-const port = 3000
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+const { engine } = require("express-handlebars");
+const app = express();
+const port = 3000;
 
-app.engine('.hbs', engine({ extname: '.hbs', defaultLayout: "layout", layoutsDir: __dirname + '/src/resources/views/layouts'}));
-app.set('view engine', '.hbs');
-app.set('views', path.join(__dirname, 'src/resources/views'));
+app.engine(
+  ".hbs",
+  engine({
+    extname: ".hbs",
+    defaultLayout: "layout",
+    layoutsDir: __dirname + "/src/resources/views/layouts",
+  })
+);
+app.set("view engine", ".hbs");
+app.set("views", path.join(__dirname, "src/resources/views"));
 
-// app.engine('handlebars', handlebars());
-// app.set('view engine', 'handlebars');
-// app.set('views', './src/resources/views');
+// fix Cannot GET /src/public/scss/style.scss
+app.use(express.static(path.join(__dirname + '/src/public')));
+
+// console.log(path.join(__dirname + '/src/public'));
 
 // http logger
-app.use(morgan('combined'))
+app.use(morgan("combined"));
 
-app.get('/', (req, res) => {
-    
-  res.render('main', { title: 'Hey', message: 'Hello there!' })
-})
+app.get("/", (req, res) => {
+  res.render("index", { title: "Learn Tech Tips", message: "Welcome to Blog spot Learn Tech Tips" });
+});
 
-app.get('/learn-tech-tips', (req, res) => {
+app.get("/learn-tech-tips", (req, res) => {
   res.send(`
     <h1 style='color:blue'> Welcome to Learn Tech Tips Channel</h1>
-  `)
-})
+  `);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
