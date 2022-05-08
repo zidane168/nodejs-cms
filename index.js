33abd79248ea77d0e71b23943cef79039099da2f
zidane <huuvi168@gmail.com>
2022-05-08 11:46:40 +0700

    add hbs engine

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const express = require('express')
+const path = require('path')
 const morgan = require('morgan')
+const { engine }  = require('express-handlebars')
 const app = express()
 const port = 3000
 
+app.engine('.hbs', engine({ extname: '.hbs', defaultLayout: "layout", layoutsDir: __dirname + '/src/resources/views/layouts'}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'src/resources/views'));
+
+// app.engine('handlebars', handlebars());
+// app.set('view engine', 'handlebars');
+// app.set('views', './src/resources/views');
+
+// http logger
 app.use(morgan('combined'))
 
 app.get('/', (req, res) => {
-    let a = 10;
-    let b = 20;
-    let c = a + b;
-  res.send('Hello World! Zidane')
+    
+  res.render('main', { title: 'Hey', message: 'Hello there!' })
+})
+
+app.get('/learn-tech-tips', (req, res) => {
+  res.send(`
+    <h1 style='color:blue'> Welcome to Learn Tech Tips Channel</h1>
+  `)
 })
 
 app.listen(port, () => {
