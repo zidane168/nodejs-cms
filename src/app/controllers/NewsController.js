1347ef4ac676dd6a437e9244047b807dbcb6b437
zidane <huuvi168@gmail.com>
2022-05-08 16:23:25 +0700

    add find model db

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,11 +1,27 @@
+const Course = require('../models/Course')
+
 class NewsController {
 
     // [GET] index 
-    index(req, res) {
-        // res.render('news');
-        res.json({
-            name: 'vilh'
-        })
+    index(req, res, next) {
+
+        Course.find({})
+            .then(courses => {
+
+                courses = courses.map( course => course.toObject()) // convert to Object for remove bug security of handlebars
+                res.render('news', { courses })
+            })
+            .catch(next);
+
+        // Course.find({}, function (err, courses) {
+        //     if (!err) {
+        //         res.render('news', { courses: courses });        
+        //         // res.json(courses);
+        //     } else {
+        //         res.status(400).json({ error: err})
+        //     }
+        // })
+      
     }
 
     test(req, res) {
